d5d7b53aabedecd3de69ca34d4a773e599513512
Trevor Maltbie <60497552+tmaltbie@users.noreply.github.com>
2020-08-31 11:23:34 -0700

    api added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import apiKey from './config'
+import SearchForm from './SearchForm';
+import Nav from './Nav';
+
+class App extends Component {
+
+  state = {
+    photos: [],
+    loading: true
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search$api_key=${apiKey}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          photos: response.data.photos,
+          loading: false
+        })
+      })
+      .catch(error => {
+        console.log('uh-oh there has been an error', error)
+      })
+  }
+
+  performSearch = (query) => {
+    
+  }
+
+  render() {
+    console.log(this.state.photos)
+    return (
+      <div className="container">
+        <SearchForm />
+        <Nav />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+
+  return (
+    <nav className="main-nav">
+      <ul>
+        <li><a href='#'>Cats</a></li>
+        <li><a href='#'>Coffee</a></li>
+        <li><a href='#'>Cows</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
